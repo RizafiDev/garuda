82ccd565f146b4e47b9cfe1497d6313d3c937b10
RizafiDev <firmansyah.riza6@smk.belajar.id>
2025-02-27 10:53:56 +0700

    add: airport database

---
+++ app/Models/Airport.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Airport extends Model
+{
+    protected $fillable = [
+        'iata_code',
+        'name',
+        'image',
+        'city',
+        'country'
+    ];
+    use HasFactory;
+}
